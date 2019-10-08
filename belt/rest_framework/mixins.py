d27113bb0b93d79e4d2e361c33a3739c79a3dcf6
Iván José Martín García <ivanmartinga@gmail.com>
2019-10-08 11:03:51 +0200

    Add simple pagination and serialization mixins

--- belt/rest_framework/mixins.py
+++ belt/rest_framework/mixins.py
@@ -1,5 +1,11 @@
+from rest_framework.serializers import Serializer
+from django.db.models import QuerySet
+from rest_framework.response import Response
+
+
 class ActionSerializersMixin:
-    """Mixin for ViewSets that allows to have a dict with a serializer for
+    """
+    Mixin for ViewSets that allows to have a dict with a serializer for
     each action.
     """
 
@@ -10,3 +16,30 @@ class ActionSerializersMixin:
             if self.action in self.action_serializers:
                 return self.action_serializers[self.action]
         return super().get_serializer_class()
+
+
+class SimpleSerializationMixin:
+    """
+    Mixin to simplify serialization of object instances.
+    """
+
+    def serialize_instance(self, data) -> Serializer:
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(instance=data)
+
+        return serializer.data
+
+
+class SimplePaginationMixin:
+    """
+    Mixin to simplify pagination.
+    """
+
+    def paginate(self, queryset: QuerySet) -> Response:
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
