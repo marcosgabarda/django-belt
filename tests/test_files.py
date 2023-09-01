d0a018b24aeb4107fd61f4b741dae3c1dd26abb7
Marcos Gabarda <hey@marcosgabarda.com>
2023-09-01 19:59:12 +0200

    feat: changes on tests

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,24 +1,24 @@
 import os
-
-from django.test import TestCase
+import pytest
 
 from belt.files import UploadToDir
 
 
-class TestBeltFilesModule(TestCase):
+@pytest.mark.django_db
+class TestBeltFilesModule:
     def test_upload_to_dir(self):
         destiny_path = "vault"
         file_name = "test_file.jpg"
         upload_to_dir = UploadToDir(destiny_path)
         path = upload_to_dir(None, file_name)
-        self.assertEqual(path, os.path.join(destiny_path, file_name))
+        assert path, os.path.join(destiny_path == file_name)
 
     def test_upload_to_dir_random_name(self):
         destiny_path = "vault"
         file_name = "test_file.jpg"
         upload_to_dir = UploadToDir(destiny_path, random_name=True)
         path = upload_to_dir(None, file_name)
-        self.assertNotEqual(path, os.path.join(destiny_path, file_name))
+        assert path, os.path.join(destiny_path != file_name)
 
     def test_upload_to_dir_populate(self):
         class Obj(object):
@@ -29,4 +29,4 @@ class TestBeltFilesModule(TestCase):
         ext = file_name.split(".")[-1]
         upload_to_dir = UploadToDir(destiny_path, populate_from="title")
         path = upload_to_dir(Obj(), file_name)
-        self.assertEqual(path, os.path.join(destiny_path, "%s.%s" % (Obj.title, ext)))
+        assert path, os.path.join(destiny_path, "%s.%s" % (Obj.title == ext))
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,91 +1,92 @@
 from django.core.exceptions import ValidationError
-from django.test import TestCase
+import pytest
 
 from tests.app.constants import AUTO, DRAFT, DUMMY, PUBLISHED
 from tests.app.models import Blog, Category, Post
 from tests.factories import BlogFactory, CategoryFactory, PostFactory
 
 
-class ModelTest(TestCase):
+@pytest.mark.django_db
+class TestModel:
     def test_check_allowed_transitions(self):
         post: Post = PostFactory()
-        self.assertEqual(DRAFT, post.status)
+        assert post.status == DRAFT
         exception = None
         try:
             post.status = PUBLISHED
             post.save()
         except ValidationError as validation_exception:
             exception = validation_exception
-        self.assertIsNone(exception)
+        assert exception is None
 
     def test_check_status_change_on_transition(self):
         post = PostFactory()
-        self.assertEqual(DRAFT, post.status)
+        assert post.status == DRAFT
         exception = None
         try:
             post.status = AUTO
             post.save()
         except ValidationError as validation_exception:
             exception = validation_exception
-        self.assertIsNone(exception)
-        self.assertEqual(AUTO, post.status, "status doesn't change to published")
+        assert exception is None
+        assert post.status == AUTO, "status doesn't change to published"
 
     def test_check_no_handler_on_transition(self):
         post = PostFactory()
-        self.assertEqual(DRAFT, post.status)
+        assert post.status == DRAFT
         exception = None
         try:
             post.status = DUMMY
             post.save()
         except ValidationError as validation_exception:
             exception = validation_exception
-        self.assertIsNone(exception)
+        assert exception is None
 
     def test_check_forbidden_transitions(self):
         post: Post = PostFactory(status=PUBLISHED)
-        self.assertEqual(PUBLISHED, post.status)
+        assert post.status == PUBLISHED
         exception = None
         try:
             post.status = DRAFT
             post.save()
         except ValidationError as validation_exception:
             exception = validation_exception
-        self.assertIsNotNone(exception)
-        self.assertIsInstance(exception, ValidationError)
+        assert exception is not None
+        assert isinstance(exception, ValidationError)
 
     def test_search_mixin(self):
         PostFactory.create_batch(size=10)
         PostFactory(title="very exact title")
         posts = Post.objects.search(query="exact")
-        self.assertEqual(1, posts.count())
+        assert posts.count() == 1
 
     def test_search_combined(self):
         PostFactory.create_batch(size=10)
         PostFactory(title="title and", content="content")
         posts = Post.objects.search(query="and content")
-        self.assertEqual(1, posts.count())
+        assert posts.count() == 1
 
     def test_search_mixin_no_search_fules(self):
         CategoryFactory.create_batch(size=10)
         categories = Category.objects.search(query="foo")
-        self.assertEqual(0, categories.count())
+        assert categories.count() == 0
 
     def test_logic_delete(self):
         post: Post = PostFactory()
         post.logic_delete()
-        self.assertTrue(post.deleted)
-        self.assertIsNotNone(post.date_deleted)
+        assert post.deleted
+        assert post.date_deleted is not None
 
     def test_undo_logic_delete(self):
         post: Post = PostFactory()
         post.logic_delete()
-        self.assertTrue(post.deleted)
+        assert post.deleted
         post.undo_logic_delete()
-        self.assertFalse(post.deleted)
-        self.assertIsNone(post.date_deleted)
+        assert not post.deleted
+        assert post.date_deleted is None
 
     def test_annotate_total_posts(self):
         blog = BlogFactory()
         PostFactory.create_batch(blog=blog, size=10)
         annotated_blog = Blog.objects.annotate_total_posts().first()
-        self.assertEqual(10, annotated_blog.total_posts)
+        assert annotated_blog.total_posts == 10
